ea8c828e118886feba8045e6cc255d351fd6b9f8
Eliasib García <eliasib.lv.12@gmail.com>
2017-03-06 21:21:04 +0000

    First commit. Implemented tip calculator using MVVM pattern.

---
+++ TipCalcXamarin/ViewModels/MainViewModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TipCalcXamarin.ViewModels
+{
+	public class MainViewModel : BindableObject
+	{
+		private decimal _total;
+		private decimal _subTotal;
+		private decimal _tipPercent;
+		private decimal _tipValue;
+		private decimal _totalPlusTax;
+
+		public decimal Total
+		{
+			get { return _total; }
+			set
+			{
+				if (_total != value)
+				{
+					_total = value;
+					Calculate();
+				}
+			}
+		}
+
+		public decimal SubTotal
+		{
+			get { return _subTotal; }
+			set
+			{
+				if (_subTotal != value)
+				{
+					_subTotal = value;
+					Calculate();
+				}
+			}
+		}
+
+		public decimal TipPercent
+		{
+			get { return _tipPercent; }
+			set
+			{
+				if (_tipPercent != value)
+				{
+					_tipPercent = value;
+					OnPropertyChanged("TipPercent");
+					Calculate();
+				}
+			}
+		}
+
+		private void Calculate()
+		{
+			if (_subTotal != 0 && _total != 0 && _tipPercent != 0)
+			{
+				TipValue = _subTotal * (_tipPercent / 100);
+				TotalPlusTax = _total + _tipValue;
+			}
+		}
+
+		public decimal TipValue
+		{
+			get
+			{
+				return _tipValue;
+			}
+			set
+			{
+				_tipValue = value;
+				OnPropertyChanged("TipValue");
+			}
+		}
+
+		public decimal TotalPlusTax
+		{
+			get { return _totalPlusTax; }
+			set
+			{
+				_totalPlusTax = value;
+				OnPropertyChanged("TotalPlusTax");
+			}
+		}
+	}
+}
---
+++ TipCalcXamarin/Views/MainView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using TipCalcXamarin.ViewModels;
+using Xamarin.Forms;
+
+namespace TipCalcXamarin
+{
+	public partial class MainView : ContentPage
+	{
+		public MainView()
+		{
+			InitializeComponent();
+
+			BindingContext = new MainViewModel();
+		}
+	}
+}
